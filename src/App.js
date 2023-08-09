e5539ab06f9f2c5cd81711eacff8dcda173a8374
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-08-09 18:55:56 +0200

    add item cart

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
           },
           
           {
-            path: "/product",
+            path: "/product/:id",
             element: <SinglProduct/>,
           }
           
--- src/components/ProductItems.jsx
+++ src/components/ProductItems.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const ProductItems = ({product}) => {
+  
+  // get title as id
+  const id = product.title.substring(0, 9)
+  // to make lowercase and remove all space
+  const strignId = (id) =>{
+      return String(id).toLowerCase().split(" ").join("")
+  }
+  console.log(strignId(id))
+  const navigate = useNavigate();
+  function handeDetail() {
+    navigate(`/product/${id}`, {
+      state:{
+        items: product,
+      }
+    })
+  }
   return (
+
+
     <div>
-
-
-
-<div class="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+<div onClick={handeDetail} class="  group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
   <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
     <img class="peer absolute top-0 right-0 h-full w-full object-cover" src={product.image} alt="product image" />
   
@@ -28,7 +44,7 @@ const ProductItems = ({product}) => {
       Show details
     </a>
     <a href="#" class="flex items-center justify-center rounded-md bg-cyan-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-      <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <svg xmlns=" " class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
       </svg>
       Add to cart
